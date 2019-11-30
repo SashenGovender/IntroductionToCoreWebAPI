63a63297ecb7a5bc4a6dea5a27e9eee52a535fa9
Sashen <sashen50@gmail.com>
2019-11-30 11:09:11 +0200

    initial commit to get started

---
+++ Controllers/PlayController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ABasicWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroductionToCoreWebAPI.Controllers
+{
+    // [Route("api/[controller]/[action]/{id?}")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayController : ControllerBase
+    {
+        private HighCard highCardGame;
+        public PlayController()
+        {
+            highCardGame = new HighCard();
+        }
+
+        //[Route("api/Play/Deal/{userID:int}/{age:int}")]
+        // public Card Deal(int userID, int age)
+
+      
+        [HttpGet]
+        [Route("Deal")]
+        //[ResponseType(typeof(Card))]
+        public ActionResult<Card> Deal()
+        {
+            Card card = highCardGame.Deal();
+            //return 5;
+            return card;
+        }
+
+        // GET api/play
+        
+        [HttpGet]
+        //[Route("api/play")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/play/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/play
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/play/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/play/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABasicWebAPI.Models
+{
+   public enum CardFace
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    };
+
+    public enum CardSuite
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs,
+    };
+
+
+    public class Card
+    {
+ 
+        public CardFace Face { get; set; }
+        public CardSuite Suite { get; set; }
+        public int Value { get; set; }
+
+        public Card(CardFace face, CardSuite suit, int value)
+        {
+            Suite = suit;
+            Face = face;
+            Value = value;
+        }
+    }
+}
